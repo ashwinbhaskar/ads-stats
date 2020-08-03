a8ed198ec356ff60f397357e353a6bc09877f488
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-03 12:13:50 +0530

    Run migrations on app start

---
+++ src/main/scala/ads.delivery/Main.scala
@@ -0,0 +1,11 @@
+package ads.delivery
+
+import com.typesafe.config.ConfigFactory
+import ads.delivery.config.AllConfigsImpl
+import ads.delivery.respository.Migration
+
+object Main extends App{
+  val tsc = ConfigFactory.load
+  val allConfigs = new AllConfigsImpl(tsc)
+  Migration.migrate(allConfigs)
+}
