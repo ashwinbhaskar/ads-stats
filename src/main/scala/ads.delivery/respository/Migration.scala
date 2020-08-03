4815855badceebd4a1591e2f60bfaef758677da5
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-03 14:38:07 +0530

    Use default location instead of custom

--- src/main/scala/ads.delivery/respository/Migration.scala
+++ src/main/scala/ads.delivery/respository/Migration.scala
@@ -7,7 +7,6 @@ object Migration {
   
     def migrate(config: DBConfig): Unit = {
         val flyway = Flyway.configure()
-            .locations("migrations")
             .dataSource(config.getUrl,config.getUser, config.getPass)
             .load()
         flyway.migrate()
