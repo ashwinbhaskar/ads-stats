f41e1d50fd955424e52b8fc2fa254056f011d17e
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-03 12:11:49 +0530

    Add migrations and resources

---
+++ src/main/resources/migrations/V1_create_delivery.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ads_delivery(
+    delivery_id UUID PRIMARY KEY,
+    advertisement_id UUID NOT NULL,
+    t TIMESTAMP WITH TIME ZONE NOT NULL,
+    browser TEXT,
+    os TEXT,
+    site TEXT NOT NULL
+)
