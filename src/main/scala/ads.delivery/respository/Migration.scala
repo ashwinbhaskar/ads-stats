594facdea966e904bca0cb6e0fb0906084891dc4
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-03 12:13:34 +0530

    Add ability to run migrations

---
+++ src/main/scala/ads.delivery/respository/Migration.scala
@@ -0,0 +1,16 @@
+package ads.delivery.respository
+
+import ads.delivery.config.DBConfig
+import org.flywaydb.core.Flyway
+
+object Migration {
+  
+    def migrate(config: DBConfig): Unit = {
+        val flyway = Flyway.configure()
+            .locations("migrations")
+            .dataSource(config.getUrl,config.getUser, config.getPass)
+            .load()
+        flyway.migrate()
+    }
+
+}
