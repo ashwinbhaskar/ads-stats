9d855474f4c51fd36d55bc6dddc9fa1666f16aec
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-03 19:38:09 +0530

    Add error types

--- src/main/scala/ads.delivery/adt/Error.scala
+++ src/main/scala/ads.delivery/adt/Error.scala
@@ -3,3 +3,11 @@ package ads.delivery.adt
 sealed trait Error {
     def message: String
 }
+
+object AlreadyRecorded extends Error {
+    override def message: String = "Delivery already recorded"
+}
+
+object UnhandledError extends Error {
+    override def message: String = "Unhandled Error"
+}
