c70fdb9d5c9168335d63dabf03f5b3c31b486402
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-05 07:42:03 +0530

    Add database

---
+++ src/main/scala/ads.delivery/respository/Database.scala
@@ -0,0 +1,31 @@
+package ads.delivery.respository
+
+import scala.concurrent.ExecutionContext
+import ads.delivery.config.DBConfig
+import cats.effect.ContextShift
+import cats.effect.{Blocker, ContextShift, IO, Resource}
+import doobie.hikari._
+import doobie.util.ExecutionContexts
+
+class Database(config: DBConfig)(implicit val ec: ExecutionContext) {
+
+  private implicit val ioContextShift: ContextShift[IO] =
+    IO.contextShift(ec)
+
+  private lazy val transactor: Resource[IO, HikariTransactor[IO]] =
+    for {
+      ce <- ExecutionContexts.fixedThreadPool[IO](config.getMaxThreads)
+      be <- Blocker[IO]
+      xa <- HikariTransactor.newHikariTransactor[IO](
+        config.getDriverClassName,
+        config.getUrl,
+        config.getUser,
+        config.getPass,
+        ce,
+        be
+      )
+    } yield xa
+
+  def getTransactor: Resource[IO, HikariTransactor[IO]] = transactor
+
+}
