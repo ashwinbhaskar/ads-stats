1b736b7aaaf92e85c395c97d95658b12b1ebec47
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-08-31 08:28:04 +0530

    Add generators for Click and Install

--- perf-test/src/main/scala/Generator.scala
+++ perf-test/src/main/scala/Generator.scala
@@ -4,14 +4,17 @@ import ads.delivery.adt.Browser
 import ads.delivery.adt.OS
 import java.net.URL
 import ads.delivery.model.Delivery
+import java.{util => ju}
+import ads.delivery.model.Click
+import ads.delivery.model.Install
 object Generator {
 
-  def url: Gen[URL] =
+  private def url: Gen[URL] =
     for {
       protocol <- Gen.oneOf[String](Seq("http", "https"))
       domain <- Gen.listOfN(20, Gen.alphaChar)
       t <- Gen.oneOf[String](Seq("com", "net", "gov", "in"))
-    } yield new URL(s"$protocol://$domain/$t")
+    } yield new URL(s"$protocol://${domain.mkString}. $t")
 
   def delivery: Gen[Delivery] =
     for {
@@ -22,4 +25,18 @@ object Generator {
       os <- Gen.oneOf[OS](OS.all)
       site <- url
     } yield Delivery(id, deliveryId, time, browser, os, site)
+  
+  def click(deliveryId: ju.UUID): Gen[Click] = 
+    for {
+      deliveryId <- Gen.const[ju.UUID](deliveryId)
+      clickId <- Gen.uuid
+      time <- Gen.posNum[Long].map(OffsetDateTimeWithMillis.fromEpochSeconds)
+    } yield Click(deliveryId, clickId, time)
+
+  def install(clickId: ju.UUID): Gen[Install] = 
+    for {
+      clickId <- Gen.const[ju.UUID](clickId)
+      installId <- Gen.uuid
+      time <- Gen.posNum[Long].map(OffsetDateTimeWithMillis.fromEpochSeconds)
+    } yield Install(installId, clickId, time)
 }
