7e7a0d46bb03dc45775df3e6bcfb1c5446da7dbd
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-09-07 12:52:44 +0530

    Use environment variable to decide the type of tracer to be used

--- src/main/scala/ads.delivery/Main.scala
+++ src/main/scala/ads.delivery/Main.scala
@@ -13,16 +13,24 @@ import ads.delivery.respository.Database
 import ads.delivery.util.Tracing
 import com.colisweb.tracing.core.TracingContextBuilder
 import cats.effect.Timer
+import com.typesafe.scalalogging.Logger
 
 object Main extends IOApp {
+
+  private val logger = Logger("Main")
 
   override def run(args: List[String]): IO[ExitCode] = {
     val tsc = ConfigFactory.load
     val configs = new AllConfigsImpl(tsc)
     Migration.migrate(configs)
-
+    
+    val shouldUseNoOpTracer = System.getenv("use_no_op_tracer") == "true"
+    logger.debug(s"is no op tracer being used? = $shouldUseNoOpTracer")
     implicit val tracingContext: TracingContextBuilder[IO] =
-      Tracing.jaegarTracingContext[IO](configs).unsafeRunSync
+      if(shouldUseNoOpTracer)
+        Tracing.noOpTracingContext[IO].unsafeRunSync
+      else
+        Tracing.jaegarTracingContext[IO](configs).unsafeRunSync
     implicit val ec = ExecutionContext.global
     val database = new Database(configs)
 
