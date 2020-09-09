3afd375fc8d5b250c39ded6aea84e1cbd656201a
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-09-10 05:18:16 +0530

    Run scalafmt

--- perf-test/src/main/scala/Main.scala
+++ perf-test/src/main/scala/Main.scala
@@ -4,22 +4,23 @@ import config.AdsStatsService
 import model.TimeTravelData
 
 object Main extends Object {
-  
+
   def timeTravel(config: AdsStatsService) = {
     val deliveries = System.getenv("deliveries").toInt
     val deliveryToClickRatio = System.getenv("delivery_to_click_ratio").toFloat
     val clickToInstallRatio = System.getenv("click_to_install_ratio").toFloat
-    val timeTravelData = TimeTravelData(deliveries, deliveryToClickRatio, clickToInstallRatio)
+    val timeTravelData =
+      TimeTravelData(deliveries, deliveryToClickRatio, clickToInstallRatio)
     AdsStatsRequests
-    .timeTravel(config, timeTravelData)
-    .unsafeRunSync
+      .timeTravel(config, timeTravelData)
+      .unsafeRunSync
   }
 
   val conf = ConfigSource.default.load[AdsStatsService]
   assert(conf.isRight)
   conf.foreach { config =>
     val isTimeTravelMode = System.getenv("is_time_travel_mode").toBoolean
-    if(isTimeTravelMode)
+    if (isTimeTravelMode)
       timeTravel(config)
   }
 
