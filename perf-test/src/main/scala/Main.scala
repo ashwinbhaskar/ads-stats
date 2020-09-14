1d0c720bf1b99ce5890e2ae17723ab4a93cd1260
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-09-14 20:27:03 +0530

    Use IOApp

--- perf-test/src/main/scala/Main.scala
+++ perf-test/src/main/scala/Main.scala
@@ -2,26 +2,35 @@ import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 import config.AdsStatsService
 import model.TimeTravelData
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
 
-object Main extends Object {
+object Main extends IOApp {
 
-  def timeTravel(config: AdsStatsService) = {
-    val deliveries = System.getenv("deliveries").toInt
-    val deliveryToClickRatio = System.getenv("delivery_to_click_ratio").toFloat
-    val clickToInstallRatio = System.getenv("click_to_install_ratio").toFloat
+  def timeTravel(config: AdsStatsService): IO[ExitCode] = {
+    val deliveries = System.getProperty("deliveries").toInt
+    val deliveryToClickRatio = System.getProperty("delivery_to_click_ratio").toFloat
+    val clickToInstallRatio = System.getProperty("click_to_install_ratio").toFloat
     val timeTravelData =
       TimeTravelData(deliveries, deliveryToClickRatio, clickToInstallRatio)
     AdsStatsRequests
       .timeTravel(config, timeTravelData)
-      .unsafeRunSync
+      .parSequence
+      .as(ExitCode.Success)
   }
 
-  val conf = ConfigSource.default.load[AdsStatsService]
-  assert(conf.isRight)
-  conf.foreach { config =>
-    val isTimeTravelMode = System.getenv("is_time_travel_mode").toBoolean
-    if (isTimeTravelMode)
-      timeTravel(config)
+  override def run(args: List[String]): IO[ExitCode] = {
+    val isTimeTravelMode = System.getProperty("is_time_travel_mode").toBoolean
+    println(s"Starting perf test in time travel mode? = $isTimeTravelMode")
+    val conf = ConfigSource.default.load[AdsStatsService]
+    conf match {
+      case Right(c) => 
+        if (isTimeTravelMode)
+          timeTravel(c)
+        else
+          ???
+      case Left(_) => IO.apply(ExitCode.Error)
+    }
   }
 
 }
