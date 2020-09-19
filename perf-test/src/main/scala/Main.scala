8bcc9f62ddca55875e982fa2c9771df1f58458b9
Ashwin Bhaskar <ashwinbhskr@gmail.com>
2020-09-19 15:52:48 +0530

    Use semaphore to restrict concurrent calls

--- perf-test/src/main/scala/Main.scala
+++ perf-test/src/main/scala/Main.scala
@@ -5,6 +5,7 @@ import model.{TimeTravelData, PerfTestData}
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
 import scala.concurrent.duration._
+import cats.effect.concurrent.Semaphore
 
 object Main extends IOApp {
 
@@ -22,6 +23,14 @@ object Main extends IOApp {
       .as(ExitCode.Success)
   }
 
+  def execute(tasks: LazyList[IO[_]], semaphore: Semaphore[IO]): IO[Unit] = {
+    for {
+      _ <- semaphore.acquire
+      _ <- IO.apply{println("aquired semaphore")}
+      _ <- tasks.parSequence
+    } yield () 
+  }
+
   def perfTest(config: AdsStatsService): IO[ExitCode] = {
     val deliveryToClickRatio =
       System.getProperty("delivery_to_click_ratio").toFloat
@@ -36,14 +45,28 @@ object Main extends IOApp {
     val perfTestData = PerfTestData(deliveryToClickRatio, clickToInstallRatio, deliveryToQueryRatio)
 
     val deadLine = timeToRunInSeconds.seconds.fromNow
-    AdsStatsRequests
-      .perfTest(config, perfTestData)
-      .parSequence
-      .takeWhile(_ => deadLine.hasTimeLeft)
-      .as(ExitCode.Success)
+    
+    val m = 
+      for {
+        s <- Semaphore[IO](3)
+        _ <- AdsStatsRequests
+              .perfTest(config, perfTestData)
+              .grouped(50)
+              .map(l => execute(l, s))
+              .takeWhile(_ => deadLine.hasTimeLeft)
+              .toList
+              .parSequence
+              .void
+      } yield ()
+
+    m.as(ExitCode.Success)
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
+    val isPerfTestMode = System.getProperty("is_perf_test_mode").toBoolean
+    if(!isPerfTestMode)
+      IO.apply(ExitCode.Success)
+    else {
     val isTimeTravelMode = System.getProperty("is_time_travel_mode").toBoolean
     println(s"Starting perf test in time travel mode? = $isTimeTravelMode")
     val conf = ConfigSource.default.load[AdsStatsService]
@@ -55,6 +78,7 @@ object Main extends IOApp {
           perfTest(c)
       case Left(_) => IO.apply(ExitCode.Error)
     }
+  }
   }
 
 }
